450bedce9ef20ee2006dc5ea8e553f307f6084a8
darya <daryanovak18@gmail.com>
2019-04-26 22:55:39 +0300

    feat: add paint bucket event

--- js/color.js
+++ js/color.js
@@ -18,15 +18,15 @@ colorPicker[0].onclick = function (e) {
 
 //color bucket 
 var canvasArea = document.getElementsByClassName('pallete__canvas-area');
+var currenttColor = document.getElementsByClassName('pallete__colors-option__current-color');
 
-cavasAre[0].onclick = function (e) {
+canvasArea[0].onclick = function (e) {
     var event = e || window.event,
         target = event.CurrentTarget || event.srcElement;
-    let prevColor = "#000"
-    if (target.tagName == 'DIV') {
-        const style = getComputedStyle(target)
-        const backgroundColor = style.backgroundColor
-        alert(backgroundColor);
-        currentColor[0].style.backgroundColor = backgroundColor;
+    if (target.tagName == 'CANVAS') {
+        var style = getComputedStyle(currenttColor[0]);
+        let bgColor = style.backgroundColor;
+        target.style.backgroundColor = bgColor;
+        alert("jw");
     }
 };
--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-var canvasArea = document.getElementsByClassName('pallete__canvas-area');
+// var canvasArea = document.getElementsByClassName('pallete__canvas-area');
 
-canvasArea[0].onclick = function (e) {
-    var event = e || window.event,
-    target = event.CurrentTarget || event.srcElement;
-    if (target.tagName == 'CANVAS') { 
-      const style = getComputedStyle(target);
-      const radius = style.borderRadius;
-      if (radius === "0px"){
-        target.style.borderRadius = "50%";
-      } else {
-        target.style.borderRadius = "0px";
-      }
-    }
-  };
+// canvasArea[0].onclick = function (e) {
+//     var event = e || window.event,
+//     target = event.CurrentTarget || event.srcElement;
+//     if (target.tagName == 'CANVAS') { 
+//       const style = getComputedStyle(target);
+//       const radius = style.borderRadius;
+//       if (radius === "0px"){
+//         target.style.borderRadius = "50%";
+//       } else {
+//         target.style.borderRadius = "0px";
+//       }
+//     }
+//   };
      
