92e2bd619f1fc6817895ccc37c2f0766c1a312d5
darya <daryanovak18@gmail.com>
2019-04-26 22:39:32 +0300

    feat: add transform event

--- js/color.js
+++ js/color.js
@@ -1,8 +1,32 @@
-var canArea = document.getElementsByClassName('pallete__canvas-area');
-canArea[0].onclick = function (e) {
-  var event = e || window.event,
-    target = event.CurrentTarget || event.srcElement;
-  if (target.tagName == 'CANVAS') {
-    target.style.borderRadius = "50%";
-  }
+// choose color
+
+var colorPicker = document.getElementsByClassName('pallete__color-picker');
+var currentColor = document.getElementsByClassName('pallete__colors-option__current-color');
+var prevColor = document.getElementsByClassName('pallete__colors-option__prev-color');
+
+colorPicker[0].onclick = function (e) {
+    var event = e || window.event,
+        target = event.CurrentTarget || event.srcElement;
+    let prevColor = "#000"
+    if (target.tagName == 'DIV') {
+        const style = getComputedStyle(target)
+        const backgroundColor = style.backgroundColor
+        alert(backgroundColor);
+        currentColor[0].style.backgroundColor = backgroundColor;
+    }
+};
+
+//color bucket 
+var canvasArea = document.getElementsByClassName('pallete__canvas-area');
+
+cavasAre[0].onclick = function (e) {
+    var event = e || window.event,
+        target = event.CurrentTarget || event.srcElement;
+    let prevColor = "#000"
+    if (target.tagName == 'DIV') {
+        const style = getComputedStyle(target)
+        const backgroundColor = style.backgroundColor
+        alert(backgroundColor);
+        currentColor[0].style.backgroundColor = backgroundColor;
+    }
 };
--- js/script.js
+++ js/script.js
@@ -1,51 +1,16 @@
+var canvasArea = document.getElementsByClassName('pallete__canvas-area');
 
-var canvas = document.getElementsByClassName('palette__canvas-area__canvas');
-var transformButton = document.getElementsByClassName('pallete__tools__transform-button');
-
-// transformButton[0].addEventListener("click", function () {
-//   Array.prototype.forEach.call(canvas, function (el) {
-//     let count = 1;
-//     el.addEventListener("click", function () {
-//       if (count % 2 == 0) {
-//         el.style.borderRadius = "50%";
-//         count++;
-//       }
-//       else {
-//         el.style.borderRadius = "0px";
-//         count++;
-//       }
-//     });
-//   });
-// });
-
-
-var canvas_area = document.getElementsByClassName('pallete__canvas-area');
-var transformButton = document.getElementsByClassName('pallete__tools__transform-button');
-
-// canvas_area[0].onclick = function (element) {
-
-//   var target = event.target;
-//   let count = 1;
-//   while (target != this) {
-//     if (target.tagName == 'CANVAS') {
-//       if (count % 2 == 0) {
-//         target.style.borderRadius = "50%";
-//         count++;
-//       }
-//       else {
-//         target.style.borderRadius = "0px";
-//         count++;
-//       }
-//     }
-//   }
-// }
-
-
-var canArea = document.getElementsByClassName('pallete__color-picker');
-canArea[0].onclick = function (e) {
-  var event = e || window.event,
+canvasArea[0].onclick = function (e) {
+    var event = e || window.event,
     target = event.CurrentTarget || event.srcElement;
-  if (target.tagName == 'CANVAS') {
-    target.style.borderRadius = "50%";
-  }
-};
+    if (target.tagName == 'CANVAS') { 
+      const style = getComputedStyle(target);
+      const radius = style.borderRadius;
+      if (radius === "0px"){
+        target.style.borderRadius = "50%";
+      } else {
+        target.style.borderRadius = "0px";
+      }
+    }
+  };
+     
