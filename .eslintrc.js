d3ae887190855248fef28314fd5b2943b1d35af0
darya <daryanovak18@gmail.com>
2019-04-30 22:41:18 +0300

    fix: change color picker logic and add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "globals": {
+    "window": true,
+    "document": true,
+    "getComputedStyle": true,
+    "Image": true,
+  }
+};
